5ad7b8eb8c85d6b70ca965dde585cb6620d5cee3
Shawn Kohltfarber <30927338+skohltfarber@users.noreply.github.com>
2019-06-14 07:13:30 -0500

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,304 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Games',
+      theme: ThemeData(
+        primaryColor: Colors.black,
+      ),
+      home: MyHomePage(title: 'Tic Tac Toe'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<GameButton> buttonsList = [];
+
+  var player1;
+  var player2;
+  var activePlayer;
+
+  @override
+  void initState() {
+    super.initState();
+    buttonsList = doInit();
+  }
+
+  List<GameButton> doInit() {
+    player1 = List();
+    player2 = List();
+    activePlayer = 1;
+
+    var gameButtons = <GameButton>[
+      GameButton(id: 1),
+      GameButton(id: 2),
+      GameButton(id: 3),
+      GameButton(id: 4),
+      GameButton(id: 5),
+      GameButton(id: 6),
+      GameButton(id: 7),
+      GameButton(id: 8),
+      GameButton(id: 9),
+    ];
+
+    return gameButtons;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: GridView.builder(
+              padding: EdgeInsets.all(10.0),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 1.0,
+                  crossAxisSpacing: 9.0,
+                  mainAxisSpacing: 9.0),
+              itemCount: buttonsList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return SizedBox(
+                  width: 100.0,
+                  height: 100.0,
+                  child: RaisedButton(
+                    padding: EdgeInsets.all(8.0),
+                    onPressed: buttonsList[index].enabled == true
+                        ? () => playGame(buttonsList[index])
+                        : null,
+                    child: Text(
+                      buttonsList[index].text,
+                      style: TextStyle(color: Colors.white, fontSize: 20.0),
+                    ),
+                    color: buttonsList[index].bg,
+                    disabledColor: buttonsList[index].bg,
+                  ),
+                );
+              },
+            ),
+          ),
+          RaisedButton(
+            padding: EdgeInsets.all(20.0),
+            child: Text("Reset"),
+            color: Colors.red,
+            onPressed: () => resetGame(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void playGame(GameButton gameButton) {
+    setState(() {
+      if (activePlayer == 1) {
+        gameButton.text = "X";
+        gameButton.bg = Colors.red;
+
+        activePlayer = 2;
+        //add button to Player1
+        player1.add(gameButton.id);
+      } else {
+        gameButton.text = "O";
+        gameButton.bg = Colors.green;
+
+        activePlayer = 1;
+        //add button to Player2
+        player2.add(gameButton.id);
+      }
+
+      gameButton.enabled = false;
+      int winner = checkWinner();
+
+      if (winner == -1) {
+        if (buttonsList.every((p) => p.text != "")) {
+          showDialog(
+              context: context,
+              builder: (_) => new CustomDialog("Tie Game",
+                  "Press the reset button to start again.", resetGame));
+        } else {
+          activePlayer == 2 ? autoPlay() : null;
+        }
+      }
+    });
+  }
+
+  void autoPlay() {
+    var emptyCells = List();
+    var list = List.generate(9, (i) => i+1);
+    for (var cellID in list) {
+      if (!(player1.contains(cellID) || player2.contains(cellID))) {
+        emptyCells.add(cellID);
+      }
+    }
+
+    var r = Random();
+    var randIndex = r.nextInt(emptyCells.length-1);
+    var cellID = emptyCells[randIndex];
+    int i = buttonsList.indexWhere((p) => p.id == cellID);
+    playGame(buttonsList[i]);
+  }
+
+  int checkWinner() {
+    var winner = -1;
+
+    if (player1.contains(1) && player1.contains(2) && player1.contains(3)) {
+      winner = 1;
+    }
+    if (player2.contains(1) && player2.contains(2) && player2.contains(3)) {
+      winner = 2;
+    }
+
+    if (player1.contains(4) && player1.contains(5) && player1.contains(6)) {
+      winner = 1;
+    }
+    if (player2.contains(4) && player2.contains(5) && player2.contains(6)) {
+      winner = 2;
+    }
+
+    if (player1.contains(7) && player1.contains(8) && player1.contains(9)) {
+      winner = 1;
+    }
+    if (player2.contains(7) && player2.contains(8) && player2.contains(9)) {
+      winner = 2;
+    }
+
+    if (player1.contains(1) && player1.contains(4) && player1.contains(7)) {
+      winner = 1;
+    }
+    if (player2.contains(1) && player2.contains(4) && player2.contains(7)) {
+      winner = 2;
+    }
+
+    if (player1.contains(2) && player1.contains(5) && player1.contains(8)) {
+      winner = 1;
+    }
+    if (player2.contains(2) && player2.contains(5) && player2.contains(8)) {
+      winner = 2;
+    }
+
+    if (player1.contains(3) && player1.contains(6) && player1.contains(9)) {
+      winner = 1;
+    }
+    if (player2.contains(3) && player2.contains(6) && player2.contains(9)) {
+      winner = 2;
+    }
+
+    if (player1.contains(1) && player1.contains(5) && player1.contains(9)) {
+      winner = 1;
+    }
+    if (player2.contains(1) && player2.contains(5) && player2.contains(9)) {
+      winner = 2;
+    }
+
+    if (player1.contains(3) && player1.contains(5) && player1.contains(7)) {
+      winner = 1;
+    }
+    if (player2.contains(3) && player2.contains(5) && player2.contains(7)) {
+      winner = 2;
+    }
+
+    if (winner != -1) {
+      if (winner == 1) {
+        showDialog(
+            context: context,
+            builder: (_) => new CustomDialog("Player 1 Won",
+                "Press the reset button to start again.", resetGame));
+      } else {
+        showDialog(
+            context: context,
+            builder: (_) => new CustomDialog("Player 2 Won",
+                "Press the reset button to start again.", resetGame));
+      }
+    }
+
+    return winner;
+  }
+
+  void resetGame() {
+    if (Navigator.canPop(context)) Navigator.pop(context);
+    setState(() {
+      buttonsList = doInit();
+    });
+  }
+}
+
+class GameButton {
+  final id;
+  String text;
+  Color bg;
+  bool enabled;
+
+  GameButton(
+      {this.id, this.text = "", this.bg = Colors.grey, this.enabled = true});
+}
+
+// class CustomDialog extends StatelessWidget {
+//   final title;
+//   final content;
+//   final VoidCallback callback;
+//   final actionText;
+
+//   CustomDialog(this.title, this.content, this.callback,
+//       [this.actionText = "Reset"]);
+//   @override
+//   Widget build(BuildContext context) {
+//     return new AlertDialog(
+//       title: new Text(title),
+//       content: new Text(content),
+//       actions: <Widget>[
+//         new FlatButton(
+//           onPressed: callback,
+//           color: Colors.white,
+//           child: new Text(actionText),
+//         )
+//       ],
+//     );
+//   }
+// }
+
+class CustomDialog extends StatelessWidget {
+  final title;
+  final content;
+  final VoidCallback callback;
+  final actionText;
+
+  CustomDialog(this.title, this.content, this.callback,
+      [this.actionText = "Reset"]);
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text(title),
+      content: Text(content),
+      actions: <Widget>[
+        RaisedButton(
+          onPressed: callback,
+          color: Colors.white,
+          child: Text(actionText),
+        )
+      ],
+    );
+  }
+}
